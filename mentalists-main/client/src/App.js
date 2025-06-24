9c40d5080435f0249ba551b8a62228a0eea3cab1
NandiniParab <parabnandini7412@gmail.com>
2025-06-24 16:22:44 +0530

    Update Founder Chairman section layout and content with animation and awards layout

--- mentalists-main/client/src/App.js
+++ mentalists-main/client/src/App.js
@@ -4,34 +4,27 @@ import { BrowserRouter, Routes, Route, useLocation } from "react-router-dom";
 import Anushka from "./pages/Anushka";
 import Homepage from "./pages/Start";
 import History from "./pages/History";
-
+import FounderChairman from "./pages/founder"; // 
 
 function App() {
   return (
-    <>
-      <BrowserRouter>
-        <RoutesWeb />
-      </BrowserRouter>
-    </>
+    <BrowserRouter>
+      <RoutesWeb />
+    </BrowserRouter>
   );
 }
 
 const RoutesWeb = () => {
-  const location = useLocation(); // Get the current route
+  const location = useLocation();
   const hideLayout = location.pathname === "/landing";
 
   return (
-    
-
-      <Routes>
-        
-        <Route path="/anushka" element={<Anushka/>} />
-        <Route path="/homepage" element={<Homepage/>} />
-        <Route path="/history" element={<History/>} />
-
-      </Routes>
-      
-    
+    <Routes>
+      <Route path="/anushka" element={<Anushka />} />
+      <Route path="/homepage" element={<Homepage />} />
+      <Route path="/history" element={<History />} />
+      <Route path="/founder" element={<FounderChairman />} /> {/* ✅ new route */}
+    </Routes>
   );
 };
 
