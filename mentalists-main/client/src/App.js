72f57e3d1362c82a0ff515d5df8d11d7c50c0111
Jiya Gudhaka <jiya.gudhaka23@spit.ac.in>
2025-06-30 15:27:14 +0530

    correction

--- mentalists-main/client/src/App.js
+++ mentalists-main/client/src/App.js
@@ -5,7 +5,7 @@ import Anushka from "./pages/Anushka";
 import Homepage from "./pages/Start";
 import History from "./pages/History";
 import FounderChairman from "./pages/FounderChairman";
-import New from "./pages/New";
+
 
 
 function App() {
@@ -24,7 +24,7 @@ const RoutesWeb = () => {
       <Route path="/homepage" element={<Homepage />} />
       <Route path="/history" element={<History />} />
       <Route path="/founder" element={<FounderChairman />} />
-      <Route path="/new" element={<New />} />
+      
       
     </Routes>
   );
