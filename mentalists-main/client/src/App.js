e62d62f037d0bb6ea0926691791e6c03f44eb03c
Jiya Gudhaka <jiya.gudhaka23@spit.ac.in>
2025-06-30 16:08:17 +0530

    final for members

--- mentalists-main/client/src/App.js
+++ mentalists-main/client/src/App.js
@@ -1,12 +1,13 @@
 import "./App.css";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route, useLocation } from "react-router-dom";
 
 import Anushka from "./pages/Anushka";
 import Homepage from "./pages/Start";
 import History from "./pages/History";
-import FounderChairman from "./pages/FounderChairman";
-
-
+import FounderChairman from "./pages/founder";
+import CCAE from "./pages/ccae"; 
+import CSAA from "./pages/csaa"; 
+import CSII from "./pages/csii"; 
 
 function App() {
   return (
@@ -17,15 +18,18 @@ function App() {
 }
 
 const RoutesWeb = () => {
+  const location = useLocation();
+  const hideLayout = location.pathname === "/landing";
+
   return (
     <Routes>
-
       <Route path="/anushka" element={<Anushka />} />
       <Route path="/homepage" element={<Homepage />} />
       <Route path="/history" element={<History />} />
       <Route path="/founder" element={<FounderChairman />} />
-      
-      
+      <Route path="/ccae" element={<CCAE />} /> 
+      <Route path="/csaa" element={<CSAA />} /> 
+      <Route path="/csii" element={<CSII />} /> 
     </Routes>
   );
 };
