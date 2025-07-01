19ed1686ea13c845f3e5d0eb7c26ac6ce5763e7c
NandiniParab <parabnandini7412@gmail.com>
2025-07-01 20:37:48 +0530

    nandini change 2

--- mentalists-main/client/src/App.js
+++ mentalists-main/client/src/App.js
@@ -1,3 +1,4 @@
+// src/App.js
 import "./App.css";
 import { BrowserRouter, Routes, Route, useLocation } from "react-router-dom";
 
@@ -5,38 +6,46 @@ import Anushka from "./pages/Anushka";
 import Homepage from "./pages/Start";
 import History from "./pages/History";
 import FounderChairman from "./pages/FounderChairman";
-import CCAE from "./pages/ccae"; 
-import CSAA from "./pages/csaa"; 
-import CSII from "./pages/csii"; 
-import Member from "./pages/Member"; 
-import SkillDevelopment from "./pages/SkillDevelopment"; 
-import COC from "./pages/coc";
+import CCAE from "./pages/ccae";
+import CSAA from "./pages/csaa";
+import CSII from "./pages/csii";
+import Member from "./pages/Member";
+import Allinone from "./pages/Allinone";
+import New from "./pages/New";
+
+// Import the TypeScript Navbar
+import Navbar from "./components/Navbar";
 
 function App() {
   return (
     <BrowserRouter>
-      <RoutesWeb />
+      <RoutesWithNavbar />
     </BrowserRouter>
   );
 }
 
-const RoutesWeb = () => {
+const RoutesWithNavbar = () => {
   const location = useLocation();
-  const hideLayout = location.pathname === "/landing";
+  const hideNavbar = location.pathname === "/landing"; // optional
 
   return (
-    <Routes>
-      <Route path="/anushka" element={<Anushka />} />
-      <Route path="/homepage" element={<Homepage />} />
-      <Route path="/history" element={<History />} />
-      <Route path="/FounderChairman" element={<FounderChairman />} />
-      <Route path="/ccae" element={<CCAE />} /> 
-      <Route path="/csaa" element={<CSAA />} /> 
-      <Route path="/csii" element={<CSII />} /> 
-      <Route path="/Member" element={<Member />} /> 
-      <Route path="/SkillDevelopment" element={<SkillDevelopment />} /> 
-      <Route path="/coc" element={<COC />} />
-    </Routes>
+    <>
+      {!hideNavbar && <Navbar />}
+      <div style={{ paddingTop: '64px' }}> {/* offset for fixed navbar */}
+        <Routes>
+          <Route path="/anushka" element={<Anushka />} />
+          <Route path="/homepage" element={<Homepage />} />
+          <Route path="/history" element={<History />} />
+          <Route path="/founderchairman" element={<FounderChairman />} />
+          <Route path="/ccae" element={<CCAE />} />
+          <Route path="/csaa" element={<CSAA />} />
+          <Route path="/csii" element={<CSII />} />
+          <Route path="/member" element={<Member />} />
+          <Route path="/allinone" element={<Allinone />} />
+          <Route path="/new" element={<New />} />
+        </Routes>
+      </div>
+    </>
   );
 };
 
