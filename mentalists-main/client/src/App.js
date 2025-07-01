68bbdc699e2a3ac25411c3ea2acdda1eeae5d881
Jiya Gudhaka <jiya.gudhaka23@spit.ac.in>
2025-07-01 23:47:52 +0530

    rukk bhai

--- mentalists-main/client/src/App.js
+++ mentalists-main/client/src/App.js
@@ -1,33 +1,58 @@
-import React from "react";
 import "./App.css";
 import { BrowserRouter, Routes, Route, useLocation } from "react-router-dom";
 
-
 import Anushka from "./pages/Anushka";
+import Homepage from "./pages/Start";
+import History from "./pages/History";
+import FounderChairman from "./pages/FounderChairman";
+import CCAE from "./pages/ccae";
+import CSAA from "./pages/csaa";
+import CSII from "./pages/csii";
+import Member from "./pages/Member";
+import Allinone from "./pages/Allinone";
+import New from "./pages/New";
+import COC from "./pages/coc";
+import PrivacyPolicy from "./pages/PrivacyPolicy";
+import TermsAndConditions from "./pages/TermsAndConditions";
+import SkillDevelopment from "./pages/SkillDevelopment";
 
-
-
-
-
+// Import the TypeScript Navbar
+import Navbar from "./components/Navbar";
 
 function App() {
   return (
     <BrowserRouter>
-      <RoutesWeb />
+      <RoutesWithNavbar />
     </BrowserRouter>
   );
 }
 
-const RoutesWeb = () => {
+const RoutesWithNavbar = () => {
   const location = useLocation();
-  const hideLayout = location.pathname === "/landing";
+  const hideNavbar = location.pathname === "/landing"; // optional
 
   return (
-    <Routes>
-      <Route path="/anushka" element={<Anushka />} />
-      
-     
-    </Routes>
+    <>
+      {!hideNavbar && <Navbar />}
+      <div style={{ paddingTop: '64px' }}> {/* offset for fixed navbar */}
+        <Routes>
+          <Route path="/anushka" element={<Anushka />} />
+          <Route path="/homepage" element={<Homepage />} />
+          <Route path="/history" element={<History />} />
+          <Route path="/founderchairman" element={<FounderChairman />} />
+          <Route path="/ccae" element={<CCAE />} />
+          <Route path="/csaa" element={<CSAA />} />
+          <Route path="/csii" element={<CSII />} />
+          <Route path="/member" element={<Member />} />
+          <Route path="/allinone" element={<Allinone />} />
+          <Route path="/new" element={<New />} />
+          <Route path="/coc" element={<COC />} />
+          <Route path="/PrivacyPolicy" element={<PrivacyPolicy />} />
+          <Route path="/TermsAndConditions" element={<TermsAndConditions />} />
+          <Route path="/SkillDevelopment" element={<SkillDevelopment />} />
+        </Routes>
+      </div>
+    </>
   );
 };
 
